dad5c87411302a363572040909b20ee76031f4af
Arthur Hu <arthurhu@Arthurs-Air.T-mobile.com>
2021-04-10 16:36:34 -0700

    can now work with individual file and recursively with directories

--- auto_encode.zsh
+++ auto_encode.zsh
@@ -1,6 +1,84 @@
+#!/bin/zsh
+
+# check if the input is video
+check_video()
+{
+    if [[ "$1" == *.mkv || "$1" == *.mp4 || "$1" == *.avi ]]; then
+        l_vid=true
+    else
+        l_vid=false
+    fi
+}
+
+# output the codec
+check_codec()
+{
+    curr_codec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name \
+        -of default=noprint_wrappers=1:nokey=1 "$1")
+}
+
+# perform encoding process into hevc
+do_encode()
+{
+    check_codec "$1"
+    if [ "$curr_codec" = "hevc" ]; then
+        echo "already hevc!"
+    else
+        f_name=$(basename "$1")
+        mkdir "$curr_dir/.cvt_tmp/"
+        /Applications/HandBrakeCLI --preset-import-file fastOCTRA.json -Z "fastOCTRA" -i "$1" -o "$curr_dir/.cvt_tmp/$f_name"
+        
+        # compare the size ... only overwrite if it's smaller
+        eval $(stat -s "$1")
+        old_size=$st_size
+        eval $(stat -s "$curr_dir/.cvt_tmp/$f_name")
+        new_size=$st_size
+        if [ $new_size -lt $old_size ]; then
+            mv "$curr_dir/.cvt_tmp/$f_name" "$1"
+        fi
+
+        # remove the temp dir
+        rm -rf "$curr_dir/.cvt_tmp/" 
+    fi
+}
+
+# put all videos in the current directory into a list
+list_allf()
+{
+
+    f_list=("${(@f)$(find $1 -type f)}")
+    for item in "${f_list[@]}"
+    do
+        check_video $item
+        if [[ "$l_vid" = true ]]; then
+            vid_list+=("$item")
+        fi 
+    done
+}
+
+trap "exit" INT
+
 read input
 
-curr_codec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name \
-        -of default=noprint_wrappers=1:nokey=1 $input)
-echo $curr_codec
+
+if [[ -f "$input" ]]; then
+    curr_dir="$(dirname "$input")"
+    
+    check_video "$input"
+    if [[ "$l_vid" = false ]]; then
+        continue 
+    else
+        do_encode "$input"
+    fi
+fi
+
+if [[ -d "$input" ]]; then
+    curr_dir="$input"
+    # put all videos in the current directory into a list
+    list_allf "$input"
+    for v_file in "${vid_list[@]}"
+    do
+        do_encode "$v_file"
+    done
+fi
 
