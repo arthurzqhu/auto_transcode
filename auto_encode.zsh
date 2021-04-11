d852859264847eaaf03833a4044b90c019c78af3
Arthur <ols.Arthur@gmail.com>
2021-04-11 15:48:09 -0700

    now supports getting duration from mkv files

--- auto_encode.zsh
+++ auto_encode.zsh
@@ -20,13 +20,12 @@ get_codec()
 # compare length - dont overwrite if they are not within a second
 cmp_dur()
 {
-    org_dur=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration \
+    org_dur=$(ffprobe -v error -select_streams v:0 -show_entries format=duration \
             -of default=noprint_wrappers=1:nokey=1 "$1")
-    new_dur=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration \
+    new_dur=$(ffprobe -v error -select_streams v:0 -show_entries format=duration \
             -of default=noprint_wrappers=1:nokey=1 "$2")
     dur_diff=$((new_dur-org_dur))
     dur_diff=${dur_diff#-} # take absolute value
-
 }
 
 # perform encoding process into hevc
