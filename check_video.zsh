279e787270527f89a6bb6ecc1767de13547c27f6
Arthur Hu <arthurhu@Arthurs-Mini.T-mobile.com>
2021-09-30 15:43:06 -0700

    more supported video format

--- check_video.zsh
+++ check_video.zsh
@@ -4,7 +4,7 @@
 check_video()
 {
     if [[ "$1" == *.mkv || "$1" == *.mp4 || "$1" == *.avi || "$1" == *.wmv \
-            || "$1" == *.mov || "$1" == *.m4v ]]; then
+            || "$1" == *.mov || "$1" == *.m4v || "$1" == *.m2ts ]]; then
         l_vid=true
     else
         l_vid=false
